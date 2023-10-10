4ae2c09206fef5dd25724f94311e4172d33d664f
cxymx17 <90310943+cxymx17@users.noreply.github.com>
2023-10-10 22:49:17 +0800

    Update login.js

--- login.js
+++ login.js
@@ -9,3 +9,18 @@ registerButton.addEventListener("click", () => {
 loginButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
+
+function updatePreview(event) {
+    const file = event.target.files[0];
+  
+    if (file) {
+      const reader = new FileReader();
+  
+      reader.onload = function(e) {
+        const preview = document.getElementById('avatar-preview');
+        preview.src = e.target.result;
+      }
+  
+      reader.readAsDataURL(file);
+    }
+  }
